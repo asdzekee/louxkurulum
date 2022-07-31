1a7b83a2af6de0a1be53c3f22fa7f207b7ea40a8
asdzekee <85474824+asdzekee@users.noreply.github.com>
2022-07-31 07:24:55 +0300

    İlk yükleyici
    Termux gerekli paketleri kurmak için kolay bir araçtır.

---
+++ termuxkurulum.py
@@ -0,0 +1,88 @@
+#!usr/bin/env python3
+
+
+import colorama
+import os
+from colorama import Fore, Back, Style
+
+os.system("pip install colorama")
+os.system("pip2 install lolcat")
+os.system("pkg install cowsay") 
+os.system("clear")
+os.system("cowsay -f eyes  Termux İlk Kuruluma Hoşgeldiniz. ~Loux~ ~~ www.turkhackteam.org ~~ | lolcat -a -d 4")
+os.system("printf 'Yüklenecek Paketler: python2 \n fish \n ruby \n php \n perl \n nmap \n bash \n clang \n nano \n curl \n tar \n zip \n unzip \n tor \n wget \n wcalc \n bmon \n golang \n openssl \n openssh \n toilet \n sl \n vim \n zsh \n fortune' | lolcat -a -d 10")
+
+
+
+
+colorama.init(autoreset=True)
+
+
+input(Fore.CYAN +Style.BRIGHT + "Kurulumu başlatmak için Enter'a basınız....")
+
+
+
+os.system("pkg update -y")
+os.system("pkg upgrade -y")
+os.system("clear")
+os.system("pkg install python")
+os.system("clear")
+os.system("pkg install python2")
+os.system("clear")
+os.system("pkg install fish -y")
+os.system("clear")
+os.system("pkg install ruby -y")
+os.system("clear")
+os.system("pkg install git -y")
+os.system("clear")
+os.system("pkg install php -y")
+os.system("clear")
+os.system("pkg install perl -y")
+os.system("clear")
+os.system("pkg install nmap -y")
+os.system("clear")
+os.system("pkg install bash -y")
+os.system("clear")
+os.system("pkg install clang -y")
+os.system("clear")
+os.system("pkg install nano -y")
+os.system("clear")
+os.system("pkg install cowsay -y")
+os.system("clear")
+os.system("pkg install curl -y")
+os.system("clear")
+os.system("pkg install tar -y")
+os.system("clear")
+os.system("pkg install zip -y")
+os.system("clear")  
+os.system("pkg install unzip -y")
+os.system("clear")
+os.system("pkg install tor -y")
+os.system("clear")
+os.system("pkg install wget -y")
+os.system("clear")
+os.system("pkg install wcalc -y")
+os.system("clear")
+os.system("pkg install bmon -y")
+os.system("clear")
+os.system("pkg install golang -y")
+os.system("clear")
+os.system("pkg install openssl -y")
+os.system("clear")
+os.system("pkg install openssh -y")
+os.system("clear")
+os.system("pkg install toilet")
+os.system("clear")
+os.system("pkg install sl")
+os.system("clear")
+os.system("pkg install vim")
+os.system("clear")
+os.system("pkg install zsh")
+os.system("clear")
+os.system("pkg install fortune")
+os.system("clear")
+os.system("apt update")
+os.system("clear")
+os.system("apt upgrade")
+
+os.system("printf 'Kurulum Bitmiştir..  ~~ www.turkhackteam.org ~~' | lolcat -a -d 70")
